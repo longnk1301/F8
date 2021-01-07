33dfab652dd36617618c78170a298dab56836cd4
Nguyen Khanh Long <khanhlong1315@gmail.com>
2021-01-07 16:05:38 +0700

    add handlebars template engine and handle

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handleBars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+//add morgan for logs http request middleware for nodejs
+app.use(morgan('combined'));
+
+//template engine
+app.engine('hbs', handleBars({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+
+app.get('/', (req, res) => {
+  // res.send('Hello World!');
+  res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  // res.send('Hello World!');
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
