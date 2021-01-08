d9a9721be22a236f669771a6bdc36460460bffbf
Nguyen Khanh Long <khanhlong1315@gmail.com>
2021-01-08 13:52:54 +0700

    add form example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const handleBars = require('express-handlebars');
 const app = express();
 const port = 3000;
@@ -8,8 +8,13 @@ const port = 3000;
 //config public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//add middleware for html
+app.use(express.urlencoded({ extended: true }));
+//add middleware for code js
+app.use(express.json());
+
 //add morgan for logs http request middleware for nodejs
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //template engine
 app.engine('hbs', handleBars({ extname: '.hbs' }));
@@ -17,13 +22,20 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
 app.get('/', (req, res) => {
-  // res.send('Hello World!');
   res.render('home');
 });
 
 app.get('/news', (req, res) => {
-  // res.send('Hello World!');
   res.render('news');
+});
+
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+
+app.post('/search', (req, res) => {
+  console.log('111', req.body);
+  res.send(req.body.q);
 });
 
 app.listen(port, () => {
